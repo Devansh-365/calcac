d5c1317a662d7767bb1e592c79f5bde773540bc4
Devansh-365 <devanshtiwari365@gmail.com>
2023-02-17 01:41:44 +0530

    feat(frontend): improve ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,31 @@ import Input from "./components/Input";
 
 function App() {
   const [post, setPost] = useState([]);
+  const [data, setData] = useState({});
+
+  const updateData = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    console.log(data);
+    axios({
+      method: "post",
+      url: "http://localhost:5000/entry",
+      data: data,
+      headers: { "Content-Type": "multipart/form-data" },
+    })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
   useEffect(() => {
     axios
@@ -20,11 +45,47 @@ function App() {
   console.log(post);
 
   return (
-    <div>
-      <header className="App-header"></header>
-      <main>
-        <Input.Text />
-      </main>
+    <div className="container">
+      <form className="form" onSubmit={submit}>
+        <header className="header">
+          <h2>Calorie Calculator</h2>
+        </header>
+        <main className="body">
+          <Input.Label>
+            Dish
+            <Input.Text name="dish" onChange={updateData} placeholder="0" />
+          </Input.Label>
+          <br />
+          <Input.Label>
+            Fat
+            <Input.Text
+              name="fat"
+              type="number"
+              onChange={updateData}
+              placeholder="0"
+            />
+          </Input.Label>
+          <br />
+          <Input.Label>
+            Ingredients
+            <Input.Text
+              name="ingredients"
+              onChange={updateData}
+              placeholder="0"
+            />
+          </Input.Label>
+          <br />
+          <Input.Label>
+            Calories
+            <Input.Text name="calories" onChange={updateData} placeholder="0" />
+          </Input.Label>
+          <br />
+          <br />
+        </main>
+        <footer>
+          <button type="submit">Submit</button>
+        </footer>
+      </form>
     </div>
   );
 }
